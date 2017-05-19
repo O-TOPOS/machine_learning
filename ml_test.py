15b4be2ad610fa72067478be97f36e121ce93565
Laurens Versluis <laurens_versluis@hotmail.com>
2017-05-19 19:55:05 +0200

    Added feature and label transformation

--- ml_test.py
+++ ml_test.py
@@ -1,12 +1,23 @@
 import numpy as np
 from sklearn import tree
-from sklearn.preprocessing import LabelEncoder
+from sklearn import cluster
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, LabelBinarizer, MinMaxScaler, StandardScaler, Normalizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pydotplus
 from IPython.display import Image
 import pydot
 import psycopg2 as ps
+import math
+import pandas
+
+# Variables
+table_name = 'test_data.ml_features_jbg'
+cat_columns = ['zonal_use', 'fp_02mtol', 'steepness', 'adj_class']
+num_columns = ['density', 'fparea', 'gutterheight', 'roofspan', 'roofcount']
+target_column = 'edb_class'
+
 
 class Database:
 
@@ -35,101 +46,61 @@ class Database:
         except Exception as e:
             print('Cannot close the connection to the server!')
 
-def get_table_data(table):
-    db = Database()
-    db.connect()
-    sql = "SELECT * FROM %s;" % table
-    db.cursor.execute(sql)
-    return db.cursor.fetchall()
 
-def get_table_header(table):
-    db = Database()
-    db.connect()
-    db.cursor.execute("Select * FROM %s" % table)
-    return [desc[0] for desc in db.cursor.description]
+class Table:
 
-def get_distinct_values(table, column):
-    db = Database()
-    db.connect()
-    db.cursor.execute("SELECT DISTINCT %s FROM %s" % (column, table))
-    values = []
-    data = db.cursor.fetchall()
-    for v in data:
-        values.append(v[0])
-    return values
+    def __init__(self, name):
+        self.db = Database()
+        self.name = name
 
-def get_encoder(data, idx):
-    feature_set = set() # Set of unique feature values
-    for record in data:
-        value = record[idx]
-        if value != None:
-            feature_set.add(record[idx])
-        else:
-            feature_set.add('') # Convert None to empty string
-    # Encode values
-    encoder = LabelEncoder()
-    encoder.fit(list(feature_set))
+    def get_table_data(self):
+        self.db.connect()
+        sql = "SELECT * FROM %s;" % self.name
+        self.db.cursor.execute(sql)
+        return self.db.cursor.fetchall()
 
-    return encoder
+    def get_table_header(self):
+        self.db.connect()
+        self.db.cursor.execute("Select * FROM %s" % self.name)
+        return [desc[0] for desc in self.db.cursor.description]
 
-def get_features_and_labels(table, id_col_idx, class_col_idx):
+    def get_distinct_values(self, column):
+        self.db.connect()
+        self.db.cursor.execute("SELECT DISTINCT %s FROM %s" % (column, self.name))
+        values = []
+        data = self.db.cursor.fetchall()
+        for v in data:
+            values.append(v[0])
+        return values
 
-    # Data containing both features and labels
-    data = get_table_data("%s" % table)
 
-    ## Generate encoders for string features
-    encoders = {}
-    for i, v in enumerate(data[0]):
-        # if i != id_col_idx:
-        #     x_name = get_table_header()
+def get_dataframe(table_name):
+    table = Table(table_name)
+    data = table.get_table_data()
+    columns = table.get_table_header()
+    return pandas.DataFrame(data=data, columns=columns)
 
-        if type(data[0][i]) == str and i != id_col_idx: # select string columns and ignore the id column
-            encoders[i] = get_encoder(data, i)
+def get_features(dataframe, num_columns, cat_columns):
+    df_num = dataframe[num_columns]
+    df_num = StandardScaler().fit_transform(df_num) # Scale numerical data
+    df_num = pandas.DataFrame(data=df_num, columns=num_columns) # Translating array back into a dataframe
+    df_cat = dataframe[cat_columns]
+    df_cat = pandas.get_dummies(data=df_cat) # Transform categorical data into dummy data
+    df_all = pandas.concat([df_num,df_cat], axis=1, join_axes=[df_num.index]) # returned joined dataframe
+    return df_all
 
-    # Fetch and transform features and labels
-    xs = []  # list of features
-    ys = []  # list of labels
+def get_labels(dataframe, target_column):
+    df_target = dataframe[target_column]
+    return LabelBinarizer().fit_transform(df_target)
 
-    for record in data:
-        if any(value is None for value in record):
-            pass # discard any record with None numerical values
-        else:
+def predict():
+    # Get the data
+    df = get_dataframe(table_name)
+    x = get_features(df, num_columns, cat_columns)
+    y = get_labels(df, target_column)
 
-            x = [] # features for current record
-            y = None # label of current record
-
-            for i, v in enumerate(record):
-
-                # Transforming the feature or label if it is a string
-                if i in encoders.keys():
-                    coded_value = encoders[i].transform([v,])
-                    if i != class_col_idx and i != id_col_idx:
-                        x.append(coded_value)
-                    elif i == class_col_idx:
-                        y = coded_value
-
-                # Otherwise just add the numerical value
-                else:
-                    if i != class_col_idx and i != id_col_idx:
-                        x.append(v)
-                    elif i == class_col_idx:
-                        y = v
-
-            # Add features and label to their lists
-            xs.append(x)
-            ys.append(y)
-
-    return xs, ys
-
-def main():
-    # Get features and labels
-    xs, ys = get_features_and_labels('test_data.ml_features', 0, 1)
-    x_names = get_table_header('test_data.ml_features')
-
-    y_names = get_distinct_values('test_data.ml_features', 'classification')
-    print(y_names)
     # Split data in a into training dataset and a test dataset
-    x_train, x_test, y_train, y_test = train_test_split(xs, ys, test_size=0.3)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 
     # Initialise classifier
     clf = tree.DecisionTreeClassifier()
@@ -141,15 +112,14 @@ def main():
     prediction = clf.predict(x_test)
 
     # Compare the accuracy of the prediction with the actual labels
-    print(accuracy_score(y_test, prediction))
+    return accuracy_score(y_test, prediction)
 
-    # Visualising tree
-    tree.export_graphviz(
-        clf,
-        out_file='tree_viz', feature_names=x_names, class_names=y_names
+def main(run_number):
+    predictions = []
+    for i in range(run_number):
+        predictions.append(predict())
 
-    )
-    # graph = pydotplus.graph_from_dot_data(dot_data)
-    # graph.write_pdf("iris.pdf")
+    print(sum(predictions)/len(predictions))
 
-main()
+
+main(10)
